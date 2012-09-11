71a1fd9a2149d8208b6a32705ee8bd76eb94ff9b
David <dtang85@gmail.com>
2012-09-10 22:58:44 -0700

    class demo update

--- week3-jQuery pt 2 and Version_Control/in-class-demos/event-delegation/demo.js
+++ week3-jQuery pt 2 and Version_Control/in-class-demos/event-delegation/demo.js
@@ -1,15 +1,18 @@
 
-(function() {
-	
+(function($) {
+	var favoriteCount = 0;
+
 	// event delegation
 	$('#contacts').on('click', '.favorite', function(e) {
 		var $this = $(this); // cache the value of $(this) in a variable
 		var name = $this.siblings('.name').text(); //DOM traversal method .siblings()
 
 		console.log(name + ' saved to favorites');
+		favoriteCount++;
+		console.log('Total favorites: ' + favoriteCount);
 
 		$this.parent().addClass('favorited'); // DOM traversal method .parent()
 		e.preventDefault(); //prevent default behavior of the link so you dont jump to the top of the page
 	});
 
-})();
+})(jQuery);
